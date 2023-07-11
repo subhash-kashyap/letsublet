e191e4a0106c130e4b6b480940709270b8e9a85e
Subhash Kashyap <sk.kashyap1997@gmail.com>
2023-07-12 02:46:15 +0530

    homescreen cleaner

--- lib/src/screens/finder/homescreen.dart
+++ lib/src/screens/finder/homescreen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -13,30 +15,51 @@ class _FinderHomeState extends State<FinderHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Title(
-          color: Colors.black,
-          child: Text("Boston, MA"),
-        ),
-        actions: [
-          Icon(Icons.star_border_outlined),
-        ],
-      ),
       body: Container(
         width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-                'assets/images/noise_bg.png'), // Replace with your image path
-            fit: BoxFit.fill,
+        // decoration: BoxDecoration(
+        //   image: DecorationImage(
+        //     image: AssetImage(
+        //         'assets/images/noise_bg.png'), // Replace with your image path
+        //     fit: BoxFit.fill,
+        //   ),
+        // ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 42.0),
+          child: Column(
+            children: [
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        log("Clicked menu");
+                      },
+                      child: Container(
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Icon(Icons.clear_all)),
+                    ),
+                    InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(color: Colors.white),
+                        child: Text("Boston, Ma"),
+                      ),
+                    ),
+                    Container(
+                        decoration: BoxDecoration(color: Colors.white),
+                        child: Icon(Icons.star_border)),
+                  ],
+                ),
+              ), // Header - Appbar replacement
+              Padding(
+                padding: const EdgeInsets.only(top: 42.0),
+                child: Container(
+                    decoration: BoxDecoration(color: Colors.white),
+                    child: Text("Bunch of Widgets")),
+              ),
+            ],
           ),
-        ),
-        child: Column(
-          children: [
-            Container(
-                decoration: BoxDecoration(color: Colors.white),
-                child: Text("Bunch of Widgets")),
-          ],
         ),
       ),
     );
